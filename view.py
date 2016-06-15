d64f6a0f77c38af27f34ca26b1e485d206af2fc3
xm <542838596@qq.com>
2016-06-15 12:47:31 +0800

    修改 不能注册多个 mapping 异常

--- view.py
+++ view.py
@@ -1,41 +1,37 @@
 __author__ = "XM"
 
 
+import functools
 from flask import Flask
 from flask import request
 from flask import render_template
-import json
+
+import jsons
+from exception import UnexpectedValueException
 
 
 app = Flask(__name__)
 
 
-def viewReslover(template,data):
+
+def viewResolver(template,data):
 	if template == None:
-		if isinstance(data,dict):
-			return json.dump(data).encode("utf-8")
-		if isinstance(data,str):
-			return data.encode('utf-8')
-		if data == None:
-			return None
-		if isinstance(data,bool):
-			string = "%s" % data
-			return string.encode('utf-8')
-		return b"some error"
+		return jsons.string(data)
 	else:
 		if isinstance(data,dict):
 			return render_template(template,**data)
+		else:
+			raise UnexpectedValueException("template need a dict value")
 
-	return None
+
 
 def view(mapping,template=None,method=["GET","POST"]):
-	print ("template  = %s" % template)
-	print ("mapping   = %s" % mapping )
 	def decorator(func):
 		@app.route(mapping,methods=method)
+		@functools.wraps(func)
 		def wrapper(*args , **kw):
 			model = func(*args,**kw)
-			return viewReslover(template,model)
+			return viewResolver(template,model)
 		return wrapper
 	return decorator
 
